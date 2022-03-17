9503997581ea6c8a2f4d04afdc729fff3591321b
Troy Weaver <troylw@gmail.com>
2022-03-17 10:41:46 -0400

    Set up user service

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using ElevenNote.Models.User;
 
 namespace ElevenNote.Services.User
 {
     public class UserService : IUserService
     {
-        
+        private readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> RegisterUserAsync(UserRegister model)
+        {
+            var entity = new UserEntity
+            {
+                Email = model.Email,
+                Username = model.Username,
+                Password = model.Password,
+                DateCreated = DateTime.Now
+            };
+            _context.Users.Add(entity);
+            var numberOfChanges = await _context.SaveChangesAsync();
+
+            return numberOfChanges == 1;
+        }
     }
 }
