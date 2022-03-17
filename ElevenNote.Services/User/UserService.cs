93fe5f7434cc75a626703e427a07478b1153f550
Troy Weaver <troylw@gmail.com>
2022-03-17 11:34:19 -0400

    Added password hashing

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using ElevenNote.Data;
 using ElevenNote.Data.Entities;
 using ElevenNote.Models.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Services.User
 {
@@ -17,17 +19,32 @@ namespace ElevenNote.Services.User
         }
         public async Task<bool> RegisterUserAsync(UserRegister model)
         {
+            if(await GetUserByEmailAsync(model.Email)!=null || await GetUserByUsernameAsync(model.Username)!= null)
+            return false;
+
             var entity = new UserEntity
             {
                 Email = model.Email,
                 Username = model.Username,
-                Password = model.Password,
+                // Password = model.Password,
                 DateCreated = DateTime.Now
             };
+
+            var passwordHasher = new PasswordHasher<UserEntity>();
+            entity.Password = passwordHasher.HashPassword(entity, model.Password);
+            
             _context.Users.Add(entity);
             var numberOfChanges = await _context.SaveChangesAsync();
 
             return numberOfChanges == 1;
+        }
+        private async Task<UserEntity> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower()== email.ToLower());
+        }
+        private async Task<UserEntity> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(user => user.Username.ToLower()== username.ToLower());
         }
     }
 }
