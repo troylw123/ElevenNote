505f9c083bc981ede143b3c24a548dd20407d1e9
Troy Weaver <troylw@gmail.com>
2022-03-17 09:38:40 -0400

    Migrations and Table set up

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Data
@@ -10,7 +11,9 @@ namespace ElevenNote.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
+
+        public DbSet<UserEntity> Users {get; set;}
     }
 }
