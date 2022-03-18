da57c7ee330b44718a28338a96bd3fe70eea29d4
Troy Weaver <troylw@gmail.com>
2022-03-18 11:13:45 -0400

    Get Note By Id method and http endpoint

--- ElevenNote.Services/Note/NoteService.cs
+++ ElevenNote.Services/Note/NoteService.cs
@@ -4,19 +4,73 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using ElevenNote.Models.Note;
+using ElevenNote.Data;
+using Microsoft.EntityFrameworkCore;
+using ElevenNote.Data.Entities;
 
 namespace ElevenNote.Services.Note
 {
     public class NoteService : INoteService
     {
         private readonly int _userId;
-        public NoteService(IHttpContextAccessor httpContextAccessor)
+        private readonly ApplicationDbContext _dbContext;
+        public NoteService(IHttpContextAccessor httpContextAccessor, ApplicationDbContext dbContext)
         {
             var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
             var value = userClaims.FindFirst("Id")?.Value;
             var validId = int.TryParse(value, out _userId);
             if (!validId) 
             throw new Exception("Attempted to build NoteService without User Id claim.");
+
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> CreateNoteAsync(NoteCreate request)
+        {
+            var noteEntity = new NoteEntity 
+            {
+                Title = request.Title,
+                Content = request.Content,
+                CreatedUtc = DateTimeOffset.Now,
+                OwnerId = _userId
+            };
+
+            _dbContext.Notes.Add(noteEntity);
+
+            var numberOfChanges = await _dbContext.SaveChangesAsync();
+            return numberOfChanges == 1;
+        }
+
+        public async Task<IEnumerable<NoteListItem>> GetAllNotesAsync()
+        {
+            var notes = await _dbContext.Notes
+                .Where(entity => entity.OwnerId == _userId)
+                .Select(entity => new NoteListItem
+                {
+                    Id = entity.Id,
+                    Title = entity.Title,
+                    CreatedUtc = entity.CreatedUtc
+                })
+                .ToListAsync();
+
+                return notes;
+        }
+        public async Task<NoteDetail> GetNoteByIdAsync(int noteId)
+        {
+            // Find the first note that has the given Id and an OwnerId that matches the requesting userId
+            var noteEntity = await _dbContext.Notes
+                .FirstOrDefaultAsync(e => e.Id == noteId && e.OwnerId == _userId);
+
+            // If noteEntity is null then return null, otherwise initialize and return a new NoteDetail
+            return noteEntity is null ? null : new NoteDetail
+            {
+                Id = noteEntity.Id,
+                Title = noteEntity.Title,
+                Content = noteEntity.Content,
+                CreatedUtc = noteEntity.CreatedUtc,
+                ModifiedUtc = noteEntity.ModifiedUtc
+            };
         }
     }
 }
---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElevenNote.Models.Note;
+using ElevenNote.Services.Note;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NoteController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+        // GET api/Note
+        [HttpGet]
+        public async Task<IActionResult> GetAllNotes()
+        {
+            var notes = await _noteService.GetAllNotesAsync();
+            return Ok(notes);
+        }
+        // Post api/Note
+        [HttpPost]
+        public async Task<IActionResult> CreateNote([FromBody] NoteCreate request)
+        {
+            if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+            if (await _noteService.CreateNoteAsync(request))
+            return Ok("Note created successfully.");
+
+            return BadRequest("Note could not be created.");
+        }
+        // Get api/Note/5
+        [HttpGet("{noteId:int}")]
+        public async Task<IActionResult> GetNoteById([FromRoute] int noteId)
+        {
+            var detail = await _noteService.GetNoteByIdAsync(noteId);
+
+            // Similar to our service method, we're using a ternary to determine our return type
+            // If the returned value (detail) is not null, return it with a 200 OK
+            // Otherwise return a NotFound() 404 response
+            return detail is not null ? Ok(detail) : NotFound();
+        }
+    }
+}
